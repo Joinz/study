92d7843ea1fab5034873bcec7d264d9ef7596391
Joinz <joinz@mail.ru>
2015-08-26 18:03:35 +0300

    Code corrected

--- homework1.js
+++ homework1.js
@@ -21,25 +21,17 @@ function createMatrix() {
 }
 
 //
-//Перемещение по матрице
+//Получить индекс div в массиве matrix по: row, col
 //
+function getIndex(row, col) {
+    return 20 * (row-1) + (col-1);
+}
 
-
 //
-//Чтение ячейки матрицы
+//Получить div по адресу: row, col
 //
-function getCell(row, col) {
-    //Функция принимает координаты ячейки,
-    //должно вернуть true, если она закрашена,
-    //false, если не закрашена
-    var num = (row-1)*20+col-1;
-    var matrixChild = matrix.children[num];
-    if (matrixChild.className = 'cell mark') {
-        return true;
-    } else {
-        return false;
-    }
-    
+function getDiv(row, col) {
+    return matrix.children[getIndex(row,col)];
 }
 
 //
@@ -49,9 +41,25 @@ function setCell(row, col, val) {
     //Функция принимает координаты ячейки,
     //если val == true, закрашивает ячейку,
     //иначе убирает закраску.
-    var num = (row-1)*20+col-1;
-    matrix.children[num].className = val ? "cell mark" : 'cell';
+    var div = getDiv(row,col);
+    div.className = val ? "cell mark" : 'cell';
 }
+
+//
+//Чтение ячейки матрицы
+//
+function getCell(row, col) {
+    //Функция принимает координаты ячейки,
+    //должно вернуть true, если она закрашена,
+    //false, если не закрашена
+    var div = getDiv(row,col);
+    return div.className.indexOf('mark') >= 0;
+}
+
+//
+//Перемещение по матрице
+//
+
 
 //
 //Точка входа
@@ -59,4 +67,26 @@ function setCell(row, col, val) {
 window.onload = function() {
     createMatrix();
     var matrix = document.getElementById('matrix');
+    var player_x;
+    var player_y;
+    var target_x;
+    var target_y;
+}
+
+//
+//Обработка нажатий клавиш
+//
+
+//
+//Запоминание нажатий клавиш
+//
+window.onkeydown = function (event) {
+    if (!event) var event = window.event;
+    var keycode;
+    if (event.keyCode) {
+        keycode = event.keyCode; //IE
+    } else if (event.which) {
+        keycode = event.which; //All browsers
+    }
+    handle(keycode);
 }
